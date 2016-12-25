440fa51f8534b2cdc085be4a3ae35dad43f580cb
Ezra Raez <ezRAez10@gmail.com>
2016-12-25 15:08:38 -0800

    give user the ability to choose amount of names

--- script.py
+++ script.py
@@ -1,10 +1,37 @@
 import random, string
 
 def generator():
-    letter1 = random.choice(string.ascii_lowercase)
-    letter2 = random.choice(string.ascii_lowercase)
-    letter3 = random.choice(string.ascii_lowercase)
-    name = letter1 + letter2 + letter3
-    return name
+    vowels = "aeiou"
+    consonants = "bcdfghjklmnpqrstvwxyz"
+    num = int(input("How many names would you like to choose from? "))
+    length_input = int(input("How long do you want the name? "))
+    choices = []
+    while length_input > 0:
+        letter_input = input("What letter do you want? Enter 'v' for vowels, 'c' for consonants, 'l' for any letter: ")
+        if letter_input == "l":
+            choices.append(letter_input)
+            length_input = length_input - 1
+        elif letter_input == "v":
+            choices.append(letter_input)
+            length_input = length_input - 1
+        elif letter_input == "c":
+            choices.append(letter_input)
+            length_input = length_input - 1
+        else:
+            pass
+    while(num > 0):
+        cat_name = ""
+        for letter in choices:
+            if letter == "l":
+                cat_name = cat_name + random.choice(string.ascii_lowercase)
+            elif letter == "v":
+                cat_name = cat_name + random.choice(vowels)
+            else:
+                cat_name = cat_name + random.choice(consonants)
+        print(cat_name)
+        num -= 1
+    return "^ Choose your favorite ^"
+
+
 
 print(generator());
